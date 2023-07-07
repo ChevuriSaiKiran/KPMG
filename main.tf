b571a14ec07f078c95f45c5c31db82f24079b4c7
ChevuriSaiKiran <137056248+ChevuriSaiKiran@users.noreply.github.com>
2023-07-07 18:24:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,137 +1,250 @@
-# Define Azure provider
-provider "azurerm" {
-  features {}
-}
-
-# Create resource group
-resource "azurerm_resource_group" "KPMG" {
-  name     = "KPMG-resource-group"
+resource "azurerm_resource_group" "rg" {
+  name     = "my-resource-group"
   location = "East US"
 }
-
-# Create Azure Virtual Network
-resource "azurerm_virtual_network" "vnet" {
-  name                = "KPMG-vnet"
+resource "azurerm_virtual_network" "my_vnet" {
+  name                = "my-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = "eastus"
-  resource_group_name = azurerm_resource_group.KPMG_rg.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
-
-# Create Azure Subnet for the app tier
+resource "azurerm_subnet" "web_subnet" {
+  name                 = "web-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.my_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
 resource "azurerm_subnet" "app_subnet" {
   name                 = "app-subnet"
-  resource_group_name  = azurerm_resource_group.KPMG_rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes      = ["10.0.1.0/24"]
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.my_vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
-# Create Azure Load Balancer for web tier
-  resource "azurerm_lb" "web_lb" {
-  name                = "web-lb"
-  location            = "eastus"
-  resource_group_name = azurerm_resource_group.KPMG_rg.name
+resource "azurerm_subnet" "db_subnet" {
+  name                 = "db-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.my_vnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
+resource "azurerm_public_ip" "lb_public_ip" {
+  name                = "lb-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+resource "azurerm_lb" "my_lb" {
+  name                = "my-lb"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.web_lb_public_ip.id
+    public_ip_address_id = azurerm_public_ip.lb_public_ip.id
   }
 }
+resource "azurerm_network_interface" "web_nic" {
+  count               = 2
+  name                = "web-nic-${count.index}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-# Create Azure Virtual Machines for web tier
-resource "azurerm_virtual_machine" "web_instances" {
-  count                 = 2
-  name                  = "web-vm-${count.index}"
-  location              = "eastus"
-  resource_group_name   = azurerm_resource_group.KPMG_rg.name
-  network_interface_ids = [azurerm_network_interface.web_nic.id]
-  vm_size               = "Standard_B1s"
+  ip_configuration {
+    name                          = "web-ipconfig"
+    subnet_id                     = azurerm_subnet.web_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+resource "azurerm_network_interface" "app_nic" {
+  count               = 2
+  name                = "app-nic-${count.index}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "app-ipconfig"
+    subnet_id                     = azurerm_subnet.app_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+resource "azurerm_network_interface" "db_nic" {
+  count               = 2
+  name                = "db-nic-${count.index}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "db-ipconfig"
+    subnet_id                     = azurerm_subnet.db_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+resource "azurerm_virtual_machine" "web_vm" {
+  count                       = 2
+  name                        = "web-vm-${count.index}"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_interface_ids       = [azurerm_network_interface.web_nic[count.index].id]
+  vm_size                     = "Standard_DS2_v2"
+  delete_os_disk_on_termination = true
+
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "16.04-LTS"
     version   = "latest"
   }
+
+  storage_os_disk {
+    name              = "web-osdisk-${count.index}"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "web-vm-${count.index}"
+    admin_username = "adminuser"
+    admin_password = "chevurisaikiran@123"
+  }
+    os_profile_linux_config {
+    disable_password_authentication = false
+  }
 }
-# Create Azure Virtual Machines for app tier
-resource "azurerm_virtual_machine" "app_instances" {
-  count                 = 2 
-  name                  = "app-vm-${count.index}"
-  location              = "eastus"
-  resource_group_name   = azurerm_resource_group.KPMG_rg.name
-  network_interface_ids = [azurerm_network_interface.app_nic.id]
-  vm_size               = "Standard_B2s"
+resource "azurerm_virtual_machine" "app_vm" {
+  count                       = 2
+  name                        = "app-vm-${count.index}"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_interface_ids       = [azurerm_network_interface.app_nic[count.index].id]
+  vm_size                     = "Standard_DS2_v2"
+  delete_os_disk_on_termination = true
+
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "16.04-LTS"
     version   = "latest"
   }
-}
-# Create Azure SQL Database for the data layer
-resource "azurerm_sql_server" "database_server" {
-  name                         = "database-server"
-  resource_group_name          = azurerm_resource_group.KPMG.name
-  location                     = azurerm_resource_group.KPMG.location
-  version                      = "12.0"
-  administrator_login          = "adminuser"
-  administrator_login_password = "password123"
-}
 
-resource "azurerm_sql_database" "database" {
-  name                         = "KPMG-db"
-  resource_group_name          = azurerm_resource_group.KPMG.name
-  location                     = azurerm_resource_group.KPMG.location
-  server_name                  = azurerm_sql_server.database_server.name
-  edition                      = "Basic"
-  requested_service_objective_name = "Basic"
-  collation                    = "SQL_Latin1_General_CP1_CI_AS"
-}
-# Create Azure Virtual Network Rule to allow access from app tier
-resource "azurerm_sql_virtual_network_rule" "app_network_rule" {
-  name                = "app-network-rule"
-  server_name         = azurerm_sql_server.database_server.name
-  resource_group_name = azurerm_resource_group.KPMG_rg.name
-  subnet_id           = azurerm_subnet.app_subnet.id
-}
+  storage_os_disk {
+    name              = "app-osdisk-${count.index}"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
 
-# Connect the web tier to the application tier
+  os_profile {
+    computer_name  = "app-vm-${count.index}"
+    admin_username = "adminuser"
+    admin_password = "chevurisaikiran@123"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
+resource "azurerm_virtual_machine" "db_vm" {
+  count                       = 2
+  name                        = "db-vm-${count.index}"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_interface_ids       = [azurerm_network_interface.db_nic[count.index].id]
+  vm_size                     = "Standard_DS2_v2"
+  delete_os_disk_on_termination = true
+
+  storage_image_reference {
+    publisher = "MicrosoftSQLServer"
+    offer     = "SQL2017-WS2016"
+    sku       = "Web"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "db-osdisk-${count.index}"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "db-vm-${count.index}"
+    admin_username = "adminuser"
+    admin_password = "chevurisaikiran@123"
+  }
+
+    os_profile_linux_config {
+    disable_password_authentication = true
+  }
+}
 resource "azurerm_lb_backend_address_pool" "web_backend_pool" {
   name                = "web-backend-pool"
-  loadbalancer_id     = azurerm_lb.web_lb.id
-  resource_group_name = azurerm_resource_group.KPMG_rg.name
-
-  backend_addresses {
-    ip_address = azurerm_virtual_machine.web_instances.*.private_ip_address
-  }
+  loadbalancer_id     = azurerm_lb.my_lb.id
 }
-
-# Connect the application tier to the database tier
-resource "azurerm_virtual_machine_extension" "app_db_extension" {
-  name                 = "app-db-extension"
-  virtual_machine_id   = azurerm_virtual_machine.app_instances[0].id
-  resource_group_name  = azurerm_resource_group.KPMG_rg.name
-  publisher            = "Microsoft.Azure.Extensions"
-  type                 = "CustomScript"
-  type_handler_version = "2.0"
-
-  settings = <<SETTINGS
-    {
-        "commandToExecute": "sudo apt-get update && sudo apt-get install -y mysql-client"
-    }
-  SETTINGS
+resource "azurerm_lb_backend_address_pool" "app_backend_pool" {
+  name                = "app-backend-pool"
+  loadbalancer_id     = azurerm_lb.my_lb.id
 }
-
-# Configure database connection string in the application tier
-resource "azurerm_virtual_machine_extension" "app_db_connection" {
-  name                 = "app-db-connection"
-  virtual_machine_id   = azurerm_virtual_machine.app_instances[0].id
-  resource_group_name  = azurerm_resource_group.KPMG_rg.name
-  publisher            = "Microsoft.Azure.Extensions"
-  type                 = "CustomScript"
-  type_handler_version = "2.0"
-
-  settings = <<SETTINGS
-    {
-        "commandToExecute": "echo 'export DB_CONNECTION_STRING=<YOUR_DATABASE_CONNECTION_STRING>' >> /etc/environment"
-    }
-  SETTINGS
+resource "azurerm_lb_backend_address_pool" "db_backend_pool" {
+  name                = "db-backend-pool"
+  loadbalancer_id     = azurerm_lb.my_lb.id
+}
+resource "azurerm_lb_probe" "web_probe" {
+  name                = "web-probe"
+  #resource_group_name = azurerm_resource_group.rg.name
+  loadbalancer_id     = azurerm_lb.my_lb.id
+  protocol            = "Tcp"
+  port                = 80
+  #interval            = 5
+  #threshold           = 2
+}
+resource "azurerm_lb_probe" "app_probe" {
+  name                = "app-probe"
+  #resource_group_name = azurerm_resource_group.rg.name
+  loadbalancer_id     = azurerm_lb.my_lb.id
+  protocol            = "Tcp"
+  port                = 8080
+  #interval            = 5
+  #threshold           = 2
+}
+resource "azurerm_lb_probe" "db_probe" {
+  name                = "db-probe"
+  #resource_group_name = azurerm_resource_group.rg.name
+  loadbalancer_id     = azurerm_lb.my_lb.id
+  protocol            = "Tcp"
+  port                = 1433
+  #interval            = 5
+  #threshold           = 2
+}
+resource "azurerm_lb_rule" "web_rule" {
+  name                           = "web-rule"
+  #resource_group_name            = azurerm_resource_group.rg.name
+  loadbalancer_id                = azurerm_lb.my_lb.id
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "PublicIPAddress"
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.web_backend_pool.id]
+  probe_id                       = azurerm_lb_probe.web_probe.id
+}
+resource "azurerm_lb_rule" "app_rule" {
+  name                           = "app-rule"
+  #resource_group_name            = azurerm_resource_group.rg.name
+  loadbalancer_id                = azurerm_lb.my_lb.id
+  protocol                       = "Tcp"
+  frontend_port                  = 8080
+  backend_port                   = 8080
+  frontend_ip_configuration_name = "PublicIPAddress"
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.app_backend_pool.id]
+  probe_id                       = azurerm_lb_probe.app_probe.id
+}
+resource "azurerm_lb_rule" "db_rule" {
+  name                           = "db-rule"
+  #resource_group_name            = azurerm_resource_group.rg.name
+  loadbalancer_id                = azurerm_lb.my_lb.id
+  protocol                       = "Tcp"
+  frontend_port                  = 1433
+  backend_port                   = 1433
+  frontend_ip_configuration_name = "PublicIPAddress"
+  backend_address_pool_ids        = [azurerm_lb_backend_address_pool.db_backend_pool.id]
+  probe_id                       = azurerm_lb_probe.db_probe.id
 }
